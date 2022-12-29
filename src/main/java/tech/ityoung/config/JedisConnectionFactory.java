d087be68886e2f70f3e77ea38063169da1fa6568
Stephen <wy1309374767@gmail.com>
2022-12-29 23:22:13 +0800

    Initial commit

---
+++ src/main/java/tech/ityoung/config/JedisConnectionFactory.java
@@ -0,0 +1,25 @@
+package tech.ityoung.config;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class JedisConnectionFactory {
+    private static final JedisPool JEDIS_POOL;
+
+    static {
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxTotal(8);
+        jedisPoolConfig.setMaxIdle(8);
+        jedisPoolConfig.setMinIdle(1);
+        jedisPoolConfig.setMaxWait(Duration.of(100L, TimeUnit.MICROSECONDS.toChronoUnit()));
+        JEDIS_POOL = new JedisPool(jedisPoolConfig, "192.168.31.11", 6379);
+    }
+
+    public static Jedis getJedis() {
+        return JEDIS_POOL.getResource();
+    }
+}
---
+++ src/test/java/tech/ityoung/JedisDemo.java
@@ -0,0 +1,62 @@
+package tech.ityoung;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPoolConfig;
+import tech.ityoung.config.JedisConnectionFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class JedisDemo {
+    private Jedis jedis;
+
+    @BeforeEach
+    void setUp() {
+        jedis = JedisConnectionFactory.getJedis();
+        jedis.select(0);
+    }
+
+    @Test
+    void testString() {
+        String result = jedis.set("name", "stephen");
+        System.out.println("result = " + result);
+
+        String name = jedis.get("name");
+        System.out.println("name = " + name);
+    }
+
+    @Test
+    void testMap() {
+        Map<String, String> userMap = new HashMap<>();
+        userMap.put("name", "stephen");
+        userMap.put("gendle", "male");
+        String user = jedis.hmset("user:1", userMap);
+        jedis.hset("user:1", "age", "28");
+        jedis.hset("user:2", "age", "28");
+        System.out.println("user = " + user);
+        List<String> fields = jedis.hmget("user:1", "name", "gendle", "age");
+        System.out.println("fields = " + fields);
+        Map<String, String> userMapResult = jedis.hgetAll("user:1");
+        System.out.println("userMapResult = " + userMapResult);
+    }
+
+    @Test
+    void testJedisPool() {
+        String result = jedis.set("name", "stephen");
+        System.out.println("result = " + result);
+
+        String name = jedis.get("name");
+        System.out.println("name = " + name);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (jedis != null) {
+            jedis.close();
+        }
+    }
+}
